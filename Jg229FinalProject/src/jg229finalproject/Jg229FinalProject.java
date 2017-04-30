02d82e9af2e68d640b24e6bb8c8019e01e33da75
Josh Green <jg229@mail.missouri.edu>
2017-04-29 19:38:22 -0500

    fixed a bunch of stuff

--- Jg229FinalProject/src/jg229finalproject/Jg229FinalProject.java
+++ Jg229FinalProject/src/jg229finalproject/Jg229FinalProject.java
@@ -5,17 +5,67 @@
  */
 package jg229finalproject;
 
+import java.util.ArrayList;
+import java.util.List;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Sahtopi
  */
-public class Jg229FinalProject {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Jg229FinalProject extends Application {
+    
+    private Pane root;
+    
+    private List<GameModel> bullets = new ArrayList<>();
+    private List<GameModel> enemies = new ArrayList<>();
+    
+    private GameModel player;
+    
+    private Parent createContent(){
+        root = new Pane();
+        root.setPrefSize(600,600);
+        
+        AnimationTimer timer = new AnimationTimer(){
+            @Override
+            public void handle(long now){
+                updateTool();
+            }
+        };
+        timer.start();
+        return root;
     }
     
+    private void updateTool(){
+        
+    }
+    
+    private static class Player extends GameModel{
+        Player(){
+            super(new Rectangle(40, 20, Color.BLACK));
+        }
+    }
+    
+    private static class Enemy extends GameModel{
+        Enemy(){
+            super(new Rectangle(40, 20, Color.WHITE));
+        }
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setScene(new Scene(createContent()));
+        stage.show();
+    }
+    
+    public static void main(String[] args){
+        launch(args);
+    }
 }
